8fdaad1e0de2d8d04125f39aa5f1e0a341e862fe
hoox276 <mebox.dev@gmail.com>
2026-04-01 20:31:01 -0300

    Muda círculo para amarelo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 const Index = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-yellow-300">
-      <div className="h-64 w-64 rounded-full bg-green-200" />
+      <div className="h-64 w-64 rounded-full bg-yellow-200" />
     </div>
   );
 };
